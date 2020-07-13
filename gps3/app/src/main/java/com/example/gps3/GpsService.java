23127af1dee7befe9ea5ca412ced5c3eb4d94497
sml <a.matyushko@gmail.com>
2020-07-13 16:30:23 +0300

    -

--- gps3/app/src/main/java/com/example/gps3/GpsService.java
+++ gps3/app/src/main/java/com/example/gps3/GpsService.java
@@ -2,26 +2,58 @@ package com.example.gps3;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.os.Process;
 import android.util.Log;
 
 public class GpsService extends Service {
-    final String LOG_TAG = "myLogs";
+    private final String LOG_TAG = "GpsService";
+    public static final int GET_DATA = 1;
+    public static final int DATA = 3;
 
+    public class GpsServiceBinder extends Binder {
+        GpsService getService() {
+            return GpsService.this;
+        }
+        public Messenger getMessanger() {
+            return new Messenger(_messanger.getBinder());
+        }
+    }
+
+    private IBinder binder = new GpsServiceBinder();
     private Gps _gps = new Gps(this);
 
+    IncomingHandler _inHandler;
+    private Messenger _messanger;
+    private Messenger _toActivityMessenger;
+
+    ///////////////////////////////////////////////////////////////
     public GpsService() {
     }
+
+    public Gps.Data gpsData() { return _gps.data(); }
 
     public void onCreate() {
         super.onCreate();
         Log.d(LOG_TAG, "onCreate");
+        HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+        _inHandler = new IncomingHandler(thread.getLooper());
+        _messanger = new Messenger(_inHandler);
+
+        _gps.callback = new GpsCallback();
         _gps.start();
     }
 
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "onStartCommand");
-        someTask();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -34,14 +66,51 @@ public class GpsService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(LOG_TAG, "onBind");
-        return null;
+//        return _messanger.getBinder();
+        return binder;
     }
 
-    void someTask() {
-//        Thread t = new Thread(new Runnable() {
-//            public void run() {
-//            }
-//        });
-//        t.start();
+    private class GpsCallback implements Gps.Callback {
+        @Override
+        public void emit() {
+            sendDataToActivity();
+        }
     }
+
+    private class IncomingHandler extends Handler {
+        public IncomingHandler(Looper looper){
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg){
+            //super.handleMessage(msg);
+
+            _toActivityMessenger = msg.replyTo;
+
+            switch (msg.what) {
+                case GET_DATA:
+                    Log.d(MainActivity.TAG, "(service)...set count");
+                    sendDataToActivity();
+                    break;
+            }
+        }
+    }
+
+    void sendDataToActivity()
+    {
+        if(_toActivityMessenger == null)
+            return;
+        Message outMsg = Message.obtain(_inHandler, DATA);
+        //outMsg.arg1 = _gps.data();
+        outMsg.replyTo = _messanger;
+
+        try {
+            _toActivityMessenger.send(outMsg);
+        }
+        catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- gps3/app/src/main/java/com/example/gps3/MainActivity.java
+++ gps3/app/src/main/java/com/example/gps3/MainActivity.java
@@ -4,7 +4,10 @@ import java.util.Date;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 
 import android.location.Location;
@@ -14,23 +17,36 @@ import android.location.GpsSatellite;
 import android.location.GpsStatus;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends Activity {
+    public static final String TAG = "MainActivity";
+
     TextView tvEnabledGPS;
     TextView tvStatusGPS;
     TextView tvLocationGPS;
     TextView textViewSatCount;
 
-    private Gps _gps = new Gps(this);
+    private GpsServiceConnection _gpsServiceConnection;
+    private GpsService _gpsService;
+    private Messenger _gpsServiceMessenger;
+    final private Messenger messenger = new Messenger(new IncomingHandler());
 
-    public class MyClass implements Gps.Callback {
+    //private Gps _gps = new Gps(this);
+
+    public class GpsCallback implements Gps.Callback {
         @Override
         public void emit() {
-            showLocation();
+            //showLocation(_gps.data());
         }
     }
 
@@ -44,20 +60,54 @@ public class MainActivity extends Activity {
         tvLocationGPS = (TextView) findViewById(R.id.tvLocationGPS);
         textViewSatCount = (TextView) findViewById(R.id.textViewSatCount);
 
-        _gps.callback = new MyClass();
+        //_gpsServiceConnection = new GpsServiceConnection();
+//        bindService(new Intent(this, GpsService.class), _gpsServiceConnection, Context.BIND_AUTO_CREATE);
+//        _gps.callback = new GpsCallback();
+    }
+
+    @Override
+    public void onDestroy() {
+        if(_gpsServiceConnection != null)
+        {
+            unbindService(_gpsServiceConnection);
+//            stopService(new Intent(this, GpsService.class));
+            _gpsServiceConnection = null;
+        }
+        super.onDestroy();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
+//        bindService(new Intent(this, GpsService.class), _gpsServiceConnection, Context.BIND_AUTO_CREATE);
 //        _gps.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+//        unbindService(_gpsServiceConnection);
 //        _gps.stop();
+    }
+
+    private class GpsServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            _gpsService = ((GpsService.GpsServiceBinder) binder).getService();
+            //_gpsServiceMessenger = new Messenger(binder);
+            _gpsServiceMessenger = ((GpsService.GpsServiceBinder) binder).getMessanger();
+            Message msg = Message.obtain(null, GpsService.GET_DATA);
+            msg.replyTo = messenger;
+            try {
+                _gpsServiceMessenger.send(msg);
+            }
+            catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {	}
     }
 
     public void onClickLocationSettings(View view) {
@@ -66,19 +116,40 @@ public class MainActivity extends Activity {
     };
 
     public void onClickButtonStart(View view) {
-        startService(new Intent(this, GpsService.class));
+        if(_gpsServiceConnection == null) {
+//        startService(new Intent(this, GpsService.class));
+            _gpsServiceConnection = new GpsServiceConnection();
+            bindService(new Intent(this, GpsService.class), _gpsServiceConnection, Context.BIND_AUTO_CREATE);
+        }
     };
 
     public void onClickButtonStop(View view) {
-        stopService(new Intent(this, GpsService.class));
+        if(_gpsServiceConnection != null)
+        {
+            unbindService(_gpsServiceConnection);
+            stopService(new Intent(this, GpsService.class));
+            _gpsServiceConnection = null;
+        }
     };
 
-    private void showLocation() {
-        Gps.Data data = _gps.data();
+    private void showLocation(Gps.Data data) {
         String s = String.format(
                 "Coordinates: lat = %1$.7f, lon = %2$.7f, alt = %3$.1f, time = %4$tF %4$tT",
                 data.lat, data.lon, data.alt, new Date(data.date));
         tvLocationGPS.setText(s);
+    }
+
+    private class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what) {
+                case GpsService.DATA:
+                    Log.d(TAG, "GpsService.DATA");
+                    Gps.Data data = _gpsService.gpsData();
+                    showLocation(data);
+                    break;
+            }
+        }
     }
 
 }
